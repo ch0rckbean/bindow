379777cc4c3d8bd31e3ad3847d65de5827e2d4db
ne0n9uy <2leebins@naver.com>
2023-09-26 17:53:59 +0900

    fix: 시간 업데이트

--- active.js
+++ active.js
@@ -1,102 +1,104 @@
 // Icons
-const galary = document.getElementById("galary");
-const imgModal = document.getElementById("imgModal");
-const comment = document.getElementById("comment");
-const cmtModal = document.getElementById("cmtModal");
-const _Btns = document.querySelectorAll("._Btn");
-const xBtns = document.querySelectorAll(".xBtn");
-const prcs1 = document.getElementById("prcs1");
-const prcs2 = document.getElementById("prcs2");
+const galary = document.getElementById('galary');
+const imgModal = document.getElementById('imgModal');
+const comment = document.getElementById('comment');
+const cmtModal = document.getElementById('cmtModal');
+const _Btns = document.querySelectorAll('._Btn');
+const xBtns = document.querySelectorAll('.xBtn');
+const prcs1 = document.getElementById('prcs1');
+const prcs2 = document.getElementById('prcs2');
 // console.log(window.innerWidth, window.innerHeight);
 //to manage modal z-index(1) : toggleModal()
 let modalArr = [];
-galary.addEventListener("click", showImgModal);
-comment.addEventListener("click", showCmtModal);
+galary.addEventListener('click', showImgModal);
+comment.addEventListener('click', showCmtModal);
 
 //to manage modal z-index(2) : toggleModal()
 for (let _Btn of _Btns) {
-  _Btn.addEventListener("click", slide);
+  _Btn.addEventListener('click', slide);
 }
 for (let xBtn of xBtns) {
-  xBtn.addEventListener("click", close);
+  xBtn.addEventListener('click', close);
 }
 
-prcs1.addEventListener("click", showImgModal);
-prcs2.addEventListener("click", showCmtModal);
+prcs1.addEventListener('click', showImgModal);
+prcs2.addEventListener('click', showCmtModal);
 
 //Comment
-const cmtRes = document.querySelector(".cmtRes");
-const cmtWrite = document.getElementById("cmtWrite");
-const cmtBtn = document.getElementById("cmtBtn");
-cmtBtn.addEventListener("click", write);
+const cmtRes = document.querySelector('.cmtRes');
+const cmtWrite = document.getElementById('cmtWrite');
+const cmtBtn = document.getElementById('cmtBtn');
+cmtBtn.addEventListener('click', write);
 
 //WindowBtn
-const windowModal = document.querySelector(".windowModal");
-const prgm = document.querySelector(".prgm");
-const help = document.querySelector(".help");
-const shtDwn = document.querySelector(".shtDwn");
-const windowBtn = document.querySelector("#windowBtn");
-windowBtn.addEventListener("click", showWindowModal);
+const windowModal = document.querySelector('.windowModal');
+const prgm = document.querySelector('.prgm');
+const help = document.querySelector('.help');
+const shtDwn = document.querySelector('.shtDwn');
+const windowBtn = document.querySelector('#windowBtn');
+windowBtn.addEventListener('click', showWindowModal);
 
 //WindowModal
-let prgmTxt = document.querySelector(".prgm p");
-prgm.addEventListener("click", function () {
-  prgmTxt.innerText = "Notion, Github, Naver Blog, Velog, Comment, Galary";
-  prgmTxt.style.color = "#E55807";
+let prgmTxt = document.querySelector('.prgm p');
+prgm.addEventListener('click', function () {
+  prgmTxt.innerText = 'Notion, Github, Naver Blog, Velog, Comment, Galary';
+  prgmTxt.style.color = '#E55807';
 });
 
-let helpTxt = document.querySelector(".help p");
-help.addEventListener("click", function () {
-  helpTxt.innerText = "Explore the Bindow!";
-  helpTxt.style.color = "#E55807";
+let helpTxt = document.querySelector('.help p');
+help.addEventListener('click', function () {
+  helpTxt.innerText = 'Explore the Bindow!';
+  helpTxt.style.color = '#E55807';
 });
 
-const off = document.querySelector(".off");
-const rtrn = document.getElementById("rtrn");
-const fadeoutClass = document.querySelector(".fadeoutClass");
-shtDwn.addEventListener("click", function () {
-  off.style.display = "block";
+const off = document.querySelector('.off');
+const rtrn = document.getElementById('rtrn');
+const fadeoutClass = document.querySelector('.fadeoutClass');
+shtDwn.addEventListener('click', function () {
+  off.style.display = 'block';
 });
-rtrn.addEventListener("click", function () {
-  off.style.display = "none";
+rtrn.addEventListener('click', function () {
+  off.style.display = 'none';
 });
 
 //Bottombar : time
-let today = new Date();
-let hr = ("0" + today.getHours()).slice(-2);
-let min = ("0" + today.getMinutes()).slice(-2);
-var timeString = hr + ":" + min;
-time.innerText = timeString;
+setInterval(() => {
+  let today = new Date();
+  let hr = ('0' + today.getHours()).slice(-2);
+  let min = ('0' + today.getMinutes()).slice(-2);
+  var timeString = hr + ':' + min;
+  time.innerText = timeString;
+}, 1000); // 1 sec
 
 //Functions
 
 function showImgModal() {
-  imgModal.style.visibility = "visible";
+  imgModal.style.visibility = 'visible';
   //   imgModal.classList.add("show");
-  console.log("show");
-  modalArr.push("imgModal");
+  console.log('show');
+  modalArr.push('imgModal');
   console.log(modalArr);
   toggleModal();
 }
 
 function showCmtModal() {
-  cmtModal.style.visibility = "visible";
-  console.log("show");
-  modalArr.push("cmtModal");
+  cmtModal.style.visibility = 'visible';
+  console.log('show');
+  modalArr.push('cmtModal');
   console.log(modalArr);
   toggleModal();
 }
 function toggleModal() {
-  if (modalArr.slice(-1)[0] == "imgModal") {
+  if (modalArr.slice(-1)[0] == 'imgModal') {
     imgModal.style.zIndex = cmtModal.style.zIndex + 1;
     console.log(imgModal.style.zIndex);
-    prcs1.style.visibility = "visible";
-    prcs1.style.display = "block";
+    prcs1.style.visibility = 'visible';
+    prcs1.style.display = 'block';
     console.log(modalArr);
-  } else if (modalArr.slice(-1)[0] == "cmtModal") {
+  } else if (modalArr.slice(-1)[0] == 'cmtModal') {
     cmtModal.style.zIndex = imgModal.style.zIndex + 1;
-    prcs2.style.visibility = "visible";
-    prcs2.style.display = "block";
+    prcs2.style.visibility = 'visible';
+    prcs2.style.display = 'block';
     // console.log(cmtModal.style.zIndex);
     console.log(modalArr);
     console.log(cmtModal.style.zIndex);
@@ -107,14 +109,14 @@ function slide() {
   console.log(modalArr);
   console.log(modalArr.slice(-1)[0]);
   //prcs2 안 먹음
-  if (modalArr.slice(-1)[0] == "imgModal") {
-    imgModal.style.visibility = "hidden";
+  if (modalArr.slice(-1)[0] == 'imgModal') {
+    imgModal.style.visibility = 'hidden';
     // console.log("slidePrcs1");
     console.log(modalArr);
     modalArr.pop();
-  } else if (modalArr.slice(-1)[0] == "cmtModal") {
+  } else if (modalArr.slice(-1)[0] == 'cmtModal') {
     // console.log("slidePrcs2");
-    cmtModal.style.visibility = "hidden";
+    cmtModal.style.visibility = 'hidden';
     modalArr.pop();
   }
   console.log(modalArr.slice(-1)[0]);
@@ -122,18 +124,18 @@ function slide() {
 
 function close() {
   //prcs2 안 먹음
-  if (modalArr.slice(-1)[0] == "imgModal") {
-    imgModal.style.visibility = "hidden";
-    prcs1.style.visibility = "hidden";
-    prcs1.style.display = "none";
+  if (modalArr.slice(-1)[0] == 'imgModal') {
+    imgModal.style.visibility = 'hidden';
+    prcs1.style.visibility = 'hidden';
+    prcs1.style.display = 'none';
     modalArr.pop();
-    console.log("closeP1");
-  } else if (modalArr.slice(-1)[0] == "cmtModal") {
-    cmtModal.style.visibility = "hidden";
-    prcs2.style.visibility = "hidden";
-    prcs2.style.display = "none";
+    console.log('closeP1');
+  } else if (modalArr.slice(-1)[0] == 'cmtModal') {
+    cmtModal.style.visibility = 'hidden';
+    prcs2.style.visibility = 'hidden';
+    prcs2.style.display = 'none';
     modalArr.pop();
-    console.log("closeP2");
+    console.log('closeP2');
   }
 }
 
@@ -141,11 +143,11 @@ function write() {
   let addText = `<p>${cmtWrite.value}</p>`;
   //   console.log(cmtWrite.value);
   cmtRes.innerHTML += addText;
-  cmtWrite.value = "";
-  console.log("wrote!");
+  cmtWrite.value = '';
+  console.log('wrote!');
 }
 function showWindowModal() {
   //   windowModal.style.visibility = "visible";
-  windowModal.classList.toggle("show");
-  console.log("windowBtn Clicked!");
+  windowModal.classList.toggle('show');
+  console.log('windowBtn Clicked!');
 }
